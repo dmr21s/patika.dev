d4bec4381695e9a306725586c54a96248e9f3677
dmr21s <demirr2144@gmail.com>
2026-04-24 15:14:42 +0300

    Add SayiSiralama class for sorting three numbers

---
+++ SayiSiralama.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SayiSiralama {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1. sayıyı girin: ");
+        int a = input.nextInt();
+
+        System.out.print("2. sayıyı girin: ");
+        int b = input.nextInt();
+
+        System.out.print("3. sayıyı girin: ");
+        int c = input.nextInt();
+
+        if (a <= b && a <= c) {
+            if (b <= c)
+                System.out.println(a + " < " + b + " < " + c);
+            else
+                System.out.println(a + " < " + c + " < " + b);
+        } else if (b <= a && b <= c) {
+            if (a <= c)
+                System.out.println(b + " < " + a + " < " + c);
+            else
+                System.out.println(b + " < " + c + " < " + a);
+        } else {
+            if (a <= b)
+                System.out.println(c + " < " + a + " < " + b);
+            else
+                System.out.println(c + " < " + b + " < " + a);
+        }
+    }
+}
