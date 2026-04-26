d809d52f5251693e76a9fcfeab51ca48fa731b74
dmr21s <demirr2144@gmail.com>
2026-04-26 22:54:10 +0300

    Clean up Fibonacci.java formatting and comments
    Removed unnecessary code comments and cleaned up formatting.

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+static int fib(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+
+    for (int i = 0; i < n; i++) {
+        System.out.print(fib(i) + " ");
+    }
+}
+
+
+}
