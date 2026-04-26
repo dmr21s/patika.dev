dd3e6c0d12342631f8192ebdbf4d60cee1d33eb8
dmr21s <demirr2144@gmail.com>
2026-04-26 23:00:36 +0300

    Implement recursive power function in Java

---
+++ RecursiveUsAlma.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RecursiveUsAlma {
+
+```
+static int power(int base, int exp) {
+    if (exp == 0)
+        return 1;
+    return base * power(base, exp - 1);
+}
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Taban değeri giriniz : ");
+    int base = input.nextInt();
+
+    System.out.print("Üs değerini giriniz : ");
+    int exp = input.nextInt();
+
+    System.out.println("Sonuç : " + power(base, exp));
+}
+```
+
+}
